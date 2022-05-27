fa375ecb41a3c5fbb07b84730f2db3a53c56334b
Radha_Singh <radha061btcsai20@igdtuw.ac.in>
2022-05-27 14:43:45 +0530

    rendering html pages

--- Flask_Blog/FlaskBlog.py
+++ Flask_Blog/FlaskBlog.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,url_for
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '15fddb6af534010669ebb40f9a4a6b37'
+
 posts = [
     {
 'author':'Corey Schafer',
@@ -24,7 +27,17 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title='Register',form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Login',form = form)
 
 if __name__=='__main__':
     app.run(debug=True)
